9db0dbd34cdcba70a31173959b7fa4a3a241d213
Vlad <borovkov-vlad@ya.ru>
2022-09-07 16:31:03 +0500

    отправка на тест

--- routes/card.js
+++ routes/card.js
@@ -16,9 +16,11 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
-      link: Joi.string().regex(
-        /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/
-      ),
+      link: Joi.string()
+        .required()
+        .regex(
+          /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/
+        ),
     }),
   }),
   createCard
