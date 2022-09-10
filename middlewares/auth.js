b0619f2a89ef2935eb826d98103982e05cd98e55
Vlad <borovkov-vlad@ya.ru>
2022-09-10 17:57:58 +0500

    feat: auth error fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,14 +5,13 @@ const UnauthorizedError = require("../errors/unauth-error");
 module.exports = (req, res, next) => {
   // достаём авторизационный заголовок
   const { authorization } = req.headers;
-
+  let payload;
   // убеждаемся, что он есть иои начинается с Bearer
   if (!authorization || !authorization.startsWith("Bearer ")) {
     next(new UnauthorizedError("Необходима авторизация"));
   } else {
     // верефицируем токен
     const token = authorization.replace("Bearer ", "");
-    let payload;
 
     payload = jwt.verify(token, "some-secret-key");
 
